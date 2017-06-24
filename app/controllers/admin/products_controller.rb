bd49d6c93053353e7fc8771744b5d78385153932
wangxing <18912540921@163.com>
2017-06-24 19:20:41 +0800

    backend CRUD

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,48 @@
+class Admin::ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_required
+  layout "admin"
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Prpduct.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.delete
+  end
+
+  private
+
+  def product_params
+    params.requrie(:product).permit(:title,:description,:price, :quantity)
+  end
+end
