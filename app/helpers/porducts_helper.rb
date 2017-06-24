beccaf266b7c06c51d8caee85ee7717b2bafb136
wangxing <18912540921@163.com>
2017-06-24 20:03:02 +0800

    product show

---
+++ app/helpers/porducts_helper.rb
@@ -0,0 +1,2 @@
+module PorductsHelper
+end
---
+++ test/controllers/porducts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PorductsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
