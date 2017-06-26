ae85e79e834dab3be42299a7e9191308e676ddac
wangxing <18912540921@163.com>
2017-06-26 19:52:39 +0800

    add apply_cancel function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,6 +44,13 @@ class OrdersController < ApplicationController
     redirect_to order_path(@order.token), notice: "使用微信付款"
   end
 
+  def apply_to_cancel
+    @order = Order.find_by_token(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已经提交申请"
+    redirect_to :back
+  end
+
 
 
   private
